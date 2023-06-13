fb1c13eabe293f41df526cb13b0459f58808f3c2
Daniel Barrientos <131481138+DanielBarrientos10@users.noreply.github.com>
2023-06-13 11:32:46 -0500

    Add files via upload

---
+++ suma.js
@@ -0,0 +1,13 @@
+
+function sumar () {
+    let numero1 = parseInt (document.getElementById("num1").value)
+    let numero2 = parseInt (document.getElementById("num2").value)
+    let suma = numero1 + numero2
+    document.getElementById ("resultado").textContent = "la suma es "+ suma;
+
+
+
+}
+               
+
+
